aea998a7e133071c19f93fd52f6092a4052957e2
Bernardo Wehmuth <bernardowehmuth1109@gmail.com>
2025-11-10 15:36:27 -0300

    feat: Refatorando arquitetura de loja controller e service

--- src/main/java/edu/marketplace/controller/LojaController.java
+++ src/main/java/edu/marketplace/controller/LojaController.java
@@ -3,13 +3,11 @@ package edu.marketplace.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import edu.marketplace.models.LojaModel;
-import edu.marketplace.models.UsuarioModel;
-import edu.marketplace.repositorys.LojaRepository;
-import edu.marketplace.repositorys.UsuarioRepository;
 import edu.marketplace.service.LojaService;
 
 
@@ -19,32 +17,20 @@ public class LojaController {
 	@Autowired
 	private LojaService lojaService;
 	
-	@Autowired
-	private LojaRepository lojaRepository;
-	
-	@Autowired 
-	private UsuarioRepository usuarioRepository;
-	
 	@GetMapping("/listarLojas")
 	public ResponseEntity<List<LojaModel>> listarLojas(){
-		return ResponseEntity.ok(lojaRepository.findAll());
+		return ResponseEntity.ok(lojaService.listarLojas());
 	}
 	
 	@PostMapping("/criarLoja/{usuarioId}")
-	public ResponseEntity<?> criarLoja(@PathVariable int usuarioId, @RequestBody LojaModel novaLoja){
-		try {
-			UsuarioModel proprietario = usuarioRepository.findById(usuarioId)
-	                .orElse(null);
-			
-			novaLoja.setProprietario(proprietario);
-			LojaModel loja = lojaService.criarLoja(novaLoja);
-			if(proprietario == null) {
-				return ResponseEntity.status(404).body("Esse usuário não Existe");
-			}
-			loja.setProprietario(proprietario);
-			return ResponseEntity.ok(loja);
-		}catch(Exception e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	public ResponseEntity<?> criarLoja(@PathVariable int usuarioId, @RequestBody LojaModel novaLoja) {
+	    try {
+	        LojaModel loja = lojaService.criarLoja(usuarioId, novaLoja);
+	        return ResponseEntity.ok(loja);
+	    } catch (IllegalArgumentException e) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+	    } catch (Exception e) {
+	        return ResponseEntity.badRequest().body(e.getMessage());
+	    }
 	}
 }
--- src/main/java/edu/marketplace/service/LojaService.java
+++ src/main/java/edu/marketplace/service/LojaService.java
@@ -1,17 +1,32 @@
 package edu.marketplace.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.marketplace.models.LojaModel;
+import edu.marketplace.models.UsuarioModel;
 import edu.marketplace.repositorys.LojaRepository;
+import edu.marketplace.repositorys.UsuarioRepository;
 
 @Service
 public class LojaService {
 	@Autowired
 	private LojaRepository lojaRepository;
 	
-	public LojaModel criarLoja(LojaModel novaLoja) {
-		return lojaRepository.save(novaLoja);
+	@Autowired 
+	private UsuarioRepository usuarioRepository;
+	
+	public List<LojaModel> listarLojas(){
+		return lojaRepository.findAll();
 	}
+	
+	public LojaModel criarLoja(int usuarioId, LojaModel novaLoja) {
+        UsuarioModel proprietario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new IllegalArgumentException("Esse usuário não existe"));
+
+        novaLoja.setProprietario(proprietario);
+        return lojaRepository.save(novaLoja);
+    }
 }
